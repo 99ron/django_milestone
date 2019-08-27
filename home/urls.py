fcd71443ed8eff77407ad7aa056503244aa9dfb2
Ubuntu <ubuntu@ip-172-31-92-143.ec2.internal>
2019-08-27 16:46:39 +0000

    Added additional filler pages (about us, contact us, home). Updated URLs and views to correspond for new template pages. Added SASS and created the scss file.

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^$', views.home, name="home"),
+    url(r'^about/$', views.about, name="about"),
+    url(r'^contact$', views.contact, name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     """A view that displays an index page"""
-    return render(request, "home/home.html")
+    return render(request, "home.html")
+
+def about(request):
+    """ A view to show the about us page"""
+    return render(request, "about.html")
+    
+def contact(request):
+    return render(request, "contact.html")

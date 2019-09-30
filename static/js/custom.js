3c184538bf011ac4e108b50445fd6df1a53d7fea
Ubuntu <ubuntu@ip-172-31-92-143.ec2.internal>
2019-09-30 00:23:51 +0000

    products/quotes.html- I've expanded on this page by adding nicer looking checkboxes and multi dropdown. Added additional form-group for car make and model, plus another for total payment and submit button at the bottom of the form.  custom.js- This has now got some scripts in to update the price on the fly on the quotes page, I'm still finalising this but currently working in my tests.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,7 @@
 /* global $ */
+
+let tos, os;
+
 // Updates the image on the quotes page to show vehicle sizing.
 $('#type-of-service').change(function(){
     var dropdown = this.value;
@@ -15,3 +18,40 @@ $('#type-of-service').change(function(){
         $("#service-image").html( "<img class='img-responsive' src='/media/quotes/Small_veh.jpg'>");
     }
  });
+
+
+// This will read what the price is for the selected items and then add it to show a total before processing.
+
+
+
+$('#type-of-service').change(function() {
+    tos = $(this).children(":selected").data("price");
+    return tos;
+});
+
+// Checks if the checkbox for the optional services on the quotes page have been
+// toggled and if so the price gets extracted and returned for the live update
+// on price.
+$('.optional-services').change(function() {
+let osArray = $('.optional-services:checked').map(function() {
+    return $(this).data('price');
+}).get();
+
+// This converts the array to be int.
+osArray = osArray.map(Number);
+
+// This takes 2 of the numbers from the conversion and adds them together
+// ready to be display in the total cost box.
+os = osArray.reduce(function(a,b){
+    return a+b}, 0);
+});
+
+
+
+$(function(){
+    setInterval(priceCheckerFunction, 1000);
+});
+
+function priceCheckerFunction() {
+  $(".total-price").html('£' + os);  
+}

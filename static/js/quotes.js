951525d209f1e6478d6ef8a37a2aa38dbb59c42b
Ubuntu <ubuntu@ip-172-31-92-143.ec2.internal>
2019-10-04 13:35:21 +0000

    Changed the name for the js file as it's just for the quotes page.

--- static/js/quotes.js
+++ static/js/quotes.js
@@ -2,20 +2,33 @@
 
 let tos, os, totalPrice;
 
-// Updates the image on the quotes page to show vehicle sizing.
+// Updates the image on the quotes page to show vehicle sizing which also hides or show's certain div's depending on choosen option.
 $('#type-of-service').change(function(){
     var dropdown = this.value;
 
     if(dropdown=="1") {
         $("#service-image").html( "<img class='img-responsive' src='/media/quotes/Large_veh.jpg'>");
-
+        $("#optional-service-container").fadeIn();
+        $("#damage-container").fadeIn();
     }
+    
     if(dropdown=="2") {
         $("#service-image").html( "<img class='img-responsive' src='/media/quotes/Mid_veh.jpg'>");
+        $("#optional-service-container").fadeIn();
+        $("#damage-container").fadeIn();
     }
         
     if(dropdown=="3") {
         $("#service-image").html( "<img class='img-responsive' src='/media/quotes/Small_veh.jpg'>");
+        $("#optional-service-container").fadeIn();
+        $("#damage-container").fadeIn();
+    }
+    
+    if(dropdown=="4") {
+        $("#service-image").html( "<img class='img-responsive' src='/media/quotes/bonnet_wrap.jpg'>");
+        $("#optional-service-container").fadeOut();
+        $("#damage-container").fadeOut();
+        
     }
  });
 
@@ -27,11 +40,11 @@ $('#type-of-service').change(function() {
     return tos;
 });
 
-    /*
-    Checks if the checkbox for the optional services on the quotes page have been
-    toggled and if so the price gets extracted and returned for the live update
-    on price. 
-    */
+/*
+Checks if the checkbox for the optional services on the quotes page have been
+toggled and if so the price gets extracted and returned for the live update
+on price. 
+*/
  
 $('.optional-services').change(function() {
 let osArray = $('.optional-services:checked').map(function() {
@@ -42,18 +55,18 @@ let osArray = $('.optional-services:checked').map(function() {
 osArray = osArray.map(Number);
 
 // This takes 2 of the numbers from the conversion and adds them together
-// ready to be display in the total cost box and if no numbers are found it defaults to 0.
+// ready to display in the total cost box and if no numbers are found it defaults to 0.
 os = osArray.reduce(function(a,b){
     return a+b}, 0);
 });
 
 
-// These are my function(s) to check on the total price which is currently updated every second.
+// This function checks and updates the total price every second.
 $(function(){
     setInterval(priceCheckerFunction, 1000);
 });
 
-
+// Checks to see what checkboxes are selected and add's the price to the total, with a few checks.
 function priceCheckerFunction() {
     if (tos > 0) {
         if (os > 0) {
@@ -67,5 +80,4 @@ function priceCheckerFunction() {
         totalPrice = 0;
         $(".total-price").val(totalPrice); 
     }
-    
 }

5e437f8e0a06d596e176b727ae688a8805aebc03
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-10-18 16:45:56 +0000

    Added the delete function. Tutor looking at possible authentication checking.

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import view_order
+from .views import view_order, delete_order
 
 urlpatterns = [
     url(r'^view/$', view_order, name='orders'),
+    url(r'^delete/(?P<order_id>\d+)/$', delete_order, name='delete'),
     ]
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -7,12 +8,13 @@ from .models import OrderList
 from products.models import Services
 from profiles.models import UserProfile
 
+ol = OrderList.objects.all()
+os = ol.filter(service_id__optional_service__isnull=False)
+
 # Create your views here.
 @login_required
 def view_order(request):
     """ A view that renders the orders contents """
-    ol = OrderList.objects.all()
-    os = ol.filter(service_id__optional_service__isnull=False)
     olf = ol.filter(username=request.user)
     user = UserProfile.objects.get(user=request.user)
     page = request.GET.get('page', 1)
@@ -22,7 +24,7 @@ def view_order(request):
     if user.employee == True:
         employee = True
         paginator = Paginator(ol, 5)
-        #print("null = False: " + str(test))
+
         try:
             olpage = paginator.page(page)
         except PageNotAnInteger:
@@ -44,3 +46,28 @@ def view_order(request):
         return render(request, "orders.html", {'orders' : olf, 'employee' : employee})
 
 
+
+@login_required
+def delete_order(request, order_id):
+    """ Deletes a selected order """
+    user = UserProfile.objects.get(user=request.user)
+    getUser = ol.filter(pk=order_id)
+    
+    
+    print(confirm)
+    confirm = getUser.service_id
+    
+    
+    if user == ol:
+        try:
+            order = OrderList.objects.filter(pk=order_id)
+            order.delete()
+            messages.success(request, "Order " + order_id + " has been removed.")
+            return redirect(view_order) 
+        except Exception as e:
+            print(e)
+            messages.error(request, "Couldn't delete object.")
+            return redirect(view_order)
+    else:
+        messages.error(request, "Couldn't delete the order, you aren't the owner.")
+        return redirect(view_order)

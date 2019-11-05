cdea1fce85e0b8d00604f6d2406cd1b92452ae12
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-05 14:35:40 +0000

    models: added blank=False, base: added block head_js tags. CSS: Additional styling for the site,

--- payment/models.py
+++ payment/models.py
@@ -11,13 +11,13 @@ numbersOnly = RegexValidator(r'^[0-9]*$', 'Only numbers are allowed.')
 class paymentDetails(models.Model):
     order = models.ForeignKey(OrderList, null=False, on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, null=False, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=30 )
-    phone_number = models.CharField(max_length=11, validators=[numbersOnly] )
-    country = models.CharField(max_length=50 )
-    postcode = models.CharField(max_length=50 )
-    town_city = models.CharField(max_length=40)
-    street_address1 = models.CharField(max_length=40 )
-    street_address2 = models.CharField(max_length=40 )
+    full_name = models.CharField(max_length=30, blank=False)
+    phone_number = models.CharField(max_length=11, validators=[numbersOnly], blank=False )
+    country = models.CharField(max_length=50, blank=False )
+    postcode = models.CharField(max_length=50, blank=False )
+    town_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False )
     
     
     def __str__(self):
---
+++ products/.~c9_invoke_rtOoWQ.py
@@ -0,0 +1,150 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# List of Manufacturers.
+
+MAKE_CHOICES = (
+    ('NA', 'Please choose a Make'),
+    ('audi','Audi'),
+    ('bmw', 'BMW'),
+    ('toyota','Toyota'),
+    ('nissan','Nissan'),
+    ('mini','Mini'),
+)
+
+MODEL_CHOICES = (
+    ('NA', 'Please choose a Model'),
+    )
+
+# Generates a unique quote number when the user submits the form. 
+
+def increment_quote_number():
+    last_quote = Services.objects.all().order_by('invoice_no').last()
+    if not last_quote:
+         return 'QUOTE_1001'
+    quote_no = last_quote.invoice_no
+    quote_int = int(quote_no.split('_')[1])
+    new_quote_int = quote_int + 1
+    new_quote_no = 'QUOTE_' + str(new_quote_int)
+    return new_quote_no
+
+# Table for types of service the company offers.
+class TypeOfService(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='quotes', blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return str(self.name)
+
+# Table for the list of colours to be used.
+class WrapColour(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='wrap_colour')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return str(self.name)
+    
+# Table for extra services the company would like to offer.
+class OptionalService(models.Model):
+    name = models.CharField(max_length=60)
+    is_needed = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    def __str__(self):
+        return str(self.name)
+
+# Table for any vehicle damage that needs to be recorded.
+class Damage(models.Model):
+    name = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    
+    def __str__(self):
+        return str(self.name)
+
+# Main table that links the others above for submitting a quote to the owner.
+class Services(models.Model):
+    invoice_no = models.CharField(max_length=500, default=increment_quote_number, null=True , blank=True)
+    type_of_service = models.ForeignKey(TypeOfService, on_delete=models.CASCADE, null=True)
+    optional_service = models.ManyToManyField(OptionalService)
+    wrap_colour = models.ForeignKey(WrapColour, on_delete=models.CASCADE, null=True)
+    damage = models.ManyToManyField(Damage)
+    damage_details = models.TextField(max_length=500, blank=True)
+    car_make = models.CharField(max_length=30, choices=MAKE_CHOICES, default='Please choose a Make', null=False)
+    car_model = models.CharField(max_length=30, choices=MODEL_CHOICES, default='Please choose a model', null=False)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    user = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return str(self.invoice_no)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

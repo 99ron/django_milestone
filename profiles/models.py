b3f51e2fe55ad08aae7433b47cb2b4336349c34e
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-07 22:17:07 +0000

    updated the requirements.txt and amended the image url's in the css files.

--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 # Table for the User profile
+
+numbersOnly = RegexValidator(r'^[0-9]*$', 'Only numbers are allowed.')
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     full_name = models.CharField(max_length=30, blank=False)
-    phone_number = models.CharField(max_length=11, blank=False)
+    phone_number = models.CharField(max_length=11, validators=[numbersOnly], blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=50, blank=False)

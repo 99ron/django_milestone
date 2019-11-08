012b7dafcdfc8aae2d0227f36499b85305b74607
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-08 23:59:00 +0000

    models: removed user field. html: Moved where service quoted from table to title location.

--- orders/models.py
+++ orders/models.py
@@ -5,7 +5,6 @@ from products.models import Services
 # Orders table.
 
 class OrderList(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     username = models.CharField(max_length=25)
     service_id = models.ForeignKey(Services, on_delete=models.CASCADE)
     paid = models.BooleanField(default=False)

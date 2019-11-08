7612d678df7d76375de404979f891964e88684c9
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-08 11:57:39 +0000

    added heroku into the allowed hosts.

--- autowrap/settings.py
+++ autowrap/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['e0d353a3a84b4c87b40b6ee5ce55763b.vfs.cloud9.us-east-1.amazonaws.com', 'https://autowraps-django-milestone.herokuapp.com/']
+ALLOWED_HOSTS = ['e0d353a3a84b4c87b40b6ee5ce55763b.vfs.cloud9.us-east-1.amazonaws.com', 'autowraps-django-milestone.herokuapp.com']
 
 
 # Application definition

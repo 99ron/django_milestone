804466eeb52f6fd0f1fa4e2536d9804fe98aac6d
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-09 00:00:52 +0000

    CSS: additional styles. product/view: check if no paint colour was selected it would choose flat black as default. profiles/view: added login_required.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,31 +1,6 @@
 
 
-<script>
-    jQuery("#delete_order").click(function(){
-        $("<div></div>").appendTo('body')
-           .html('<div><h3> Are you sure you want to delete this order? </h3></div>')
-           .dialog({
-                title: "Confirm" ,
-                width:500, height:300,
-                modal:true,
-                resizable: false, 
-                show: { effect: 'drop', direction: "left" }, 
-                hide:{effect:'blind'}
-
-                buttons: {
-                    Yes: function() {
-                          jQuery.ajax({
-                              type:"POST", //post data
-                              data:{'key':key}, //if you want to send any data to view 
-                              url:'/delete/' // your url that u write in action in form tag
-                          }).done(function(result){
-                               alert("am done") //this will executes after your view executed  
-                          })
-                     },
-                    Cancel: function() {
-                        $( this ).dialog( "close" );
-                    }
-               }
-           });
-    });
-<script> 
+// This shows a basic prompt to whether the user wants to go ahead with their decision.
+$(document).on('click', '.confirm-delete', function(){
+    return confirm('Are you sure you want to delete this?');
+})

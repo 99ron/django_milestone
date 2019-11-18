ae9eca0b2a6f09298df27ecda93ae5a6d8baff37
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-18 21:42:10 +0000

    Added new query for displaying orders list that if a review has been left on an order it will exclude it from being rendered. Commented on my code, more to add.

--- orders/views.py
+++ orders/views.py
@@ -2,38 +2,29 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import OrderList
 from products.models import Services
 from profiles.models import UserProfile
-
-ol = OrderList.objects.all()
+from gallery.models import Reviews
 
 # Orders page views.
 
 @login_required
 def view_order(request):
     """ A view that renders the orders contents """
-    olf = ol.filter(username=request.user)
+    review_true = Reviews.objects.all().values_list('order_number', flat=True)
+    ol = OrderList.objects.exclude(pk__in=review_true)
+
+    olf = ol.filter(username=request.user).exclude(pk__in=review_true)
     user = UserProfile.objects.get(user=request.user)
     page = request.GET.get('page', 1)
-    
-    
+
     # Checks to see if the logged in user is an employee or not.
     if user.employee == True:
-        employee = True
+        
         paginator = Paginator(ol, 5)
-
-        try:
-            olpage = paginator.page(page)
-        except PageNotAnInteger:
-            olpage = paginator.page(1)
-        except EmptyPage:
-            olpage = paginator.page(paginator.num_pages)
-            
-        return render(request, "orders.html", {'orders' : olpage, 'employee' : employee})
-    else:
-        paginator = Paginator(olf, 5)
+        employee = True
         try:
             olpage = paginator.page(page)
         except PageNotAnInteger:
@@ -41,7 +32,18 @@ def view_order(request):
         except EmptyPage:
             olpage = paginator.page(paginator.num_pages)
         
+        return render(request, "orders.html", {'orders' : olpage, 'employee' : employee})
+    
+    else:
+        paginator = Paginator(olf, 5)
         employee = False
+        try:
+            olpage = paginator.page(page)
+        except PageNotAnInteger:
+            olpage = paginator.page(1)
+        except EmptyPage:
+            olpage = paginator.page(paginator.num_pages)
+        
         return render(request, "orders.html", {'orders' : olf, 'employee' : employee})
 
 

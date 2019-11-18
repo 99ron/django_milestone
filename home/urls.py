cd935371f04bed9ce2f5540b8a7102491d8d6481
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-18 21:52:29 +0000

    Added comment to my code for the home app.

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from . import views
 
+# URLpatterns for the home app.
 urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^about/$', views.about, name="about"),
--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views for the home app.
+
 def home(request):
     """A view that displays an index page"""
     return render(request, "home.html")
-
+    
 def about(request):
     """ A view to show the about us page"""
     return render(request, "about.html")

a6e4fa7d61659465f2e1c776ffdc8a3dc8770519
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-19 16:19:21 +0000

    Added a scroll to top button.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,3 +5,17 @@ $(document).on('click', '.confirm-delete', function(){
     return confirm('Are you sure you want to delete this?');
 });
 
+// Function for scroll to top. Fades in when greater than 100px from the top.
+$(window).scroll(function(){ 
+    if ($(this).scrollTop() > 100) { 
+        $('#scroll').fadeIn(); 
+    } else { 
+        $('#scroll').fadeOut(); 
+    } 
+}); 
+
+// Once button is clicked it'll scroll to the top.
+$('#scroll').click(function(){ 
+    $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+});

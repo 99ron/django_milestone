c6b62b64b64c63a68382acf27872cd91d0e3b6b3
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-22 21:36:06 +0000

    orders.html: Changed the button for the 'Show more' on the optional services and damage container so it has an ID and onclick link to some JS so the text changes when it's clicked.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,8 @@ $(document).on('click', '.confirm-delete', function(){
     return confirm('Are you sure you want to delete this?');
 });
 
+
+
 // Function for scroll to top. Fades in when greater than 100px from the top.
 $(window).scroll(function(){ 
     if ($(this).scrollTop() > 100) { 
@@ -14,8 +16,44 @@ $(window).scroll(function(){
     } 
 }); 
 
+
+
 // Once button is clicked it'll scroll to the top.
 $('#scroll').click(function(){ 
     $("html, body").animate({ scrollTop: 0 }, 600); 
     return false; 
 });
+
+
+
+// This function changes the text between 'More Options' and 'Show Less'.
+function ShowMoreChange(id) {
+    if(id == "ShowMoreOptional") {
+       let SMO = document.getElementById('ShowMoreOptional');
+        if (SMO.innerHTML == "More Options") { 
+            SMO.innerHTML ="Show Less"; 
+        } else { 
+            SMO.innerHTML = "More Options"; 
+        }
+    }
+    
+    // This is for the damage container on the quotes and edit pages.
+    if(id == "ShowMoreDamage") {
+        let SMD = document.getElementById('ShowMoreDamage');
+        if (SMD.innerHTML == "More Options") { 
+            SMD.innerHTML ="Show Less"; 
+        } else { 
+            SMD.innerHTML = "More Options"; 
+        }
+    }
+    
+    // This is for the orders page.
+    if(id == "ShowMoreOrder") {
+        let SMO = document.getElementById('ShowMoreOrder');
+        if (SMO.innerHTML == "Show More") { 
+            SMO.innerHTML ="Show Less"; 
+        } else { 
+            SMO.innerHTML = "Show More"; 
+        }
+    }
+}

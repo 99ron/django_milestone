9c74350eb6df152ac433a318f4fac977f141f1d2
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-11-26 10:32:18 +0000

    old test that wasn't used.

--- products/tests.py
+++ products/tests.py
@@ -3,13 +3,4 @@ from .models import Product
 
 # Create your tests here.
 
-class ProductTests(TestCase):
-    """
-    Here we will define the tests 
-    that we'll run against our Product models
-    """
-    
-    def test_str(self):
-        test_name = Product(name='A Product')
-        self.assertEqual(str(test_name), 'A Product')
         

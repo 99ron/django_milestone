0719825774b4b00bfbe286af729b2dab013270c6
99ron <ubuntu@ip-172-31-92-143.ec2.internal>
2019-12-10 11:26:02 +0000

    Removing the .c9 files which managed to get uploaded on a previous commit.

--- contactus/.~c9_invoke_ns8rcI.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-from .views import contact, successView
-
-urlpatterns = [
-    url(r'success/', successView, name='success'),
-    url(r'^message/$', contact, name="contact"),
-]
--- profiles/.~c9_invoke_LhARyu.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, include
-from profiles.views import user_profile
-
-urlpatterns = [
-    url(r'^profile/$', user_profile, name="profile"),
-]
